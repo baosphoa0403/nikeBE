1baf7e2a771f249b74f00fe7d119e95be819567f
baosphoa0403 <baosphoa0403@gmail.com>
2021-07-14 12:46:09 +0700

    set up configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,13 +4,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductModule } from './product/product.module';
 import { StatusModule } from './status/status.module';
-
 @Module({
   imports: [
     ProductModule,
-    MongooseModule.forRoot(
-      'mongodb+srv://giabao:dXrQuNHl0gzHaGdp@cluster0.ud7sx.mongodb.net/managementShoes?retryWrites=true&w=majority',
-    ),
+    MongooseModule.forRoot(process.env.DATABASE_URL),
     StatusModule,
   ],
   controllers: [AppController],
