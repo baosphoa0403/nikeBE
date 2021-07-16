1d2dea989c0c00d6273fbc4212295666240929fe
CNDvn <banhduchieucode@gmail.com>
2021-07-17 01:00:53 +0700

    add custom validator for Bao ngu

--- src/role/dto/createRole.dto.ts
+++ src/role/dto/createRole.dto.ts
@@ -1,3 +1,9 @@
+import { Transform } from "class-transformer";
+import { IsNotEmpty } from "class-validator";
+import { IsNotBlank } from "src/custom-validator/is-not-blank.validator";
+
 export class CreateRoleDto{
+
+    @IsNotBlank('nameRole',{message: 'Role name is not empty'})
     nameRole: string;
 }
