2923ca2fabce1089d2a3febaff8f42e8170c58e6
nnlam-batch32 <nnlam-batch32@sdc.edu.vn>
2021-07-18 09:48:04 +0700

    edit validation

--- src/size/dto/update-size.dto.ts
+++ src/size/dto/update-size.dto.ts
@@ -1,6 +1,8 @@
 import { PartialType } from '@nestjs/mapped-types';
+import { IsNotEmpty } from 'class-validator';
 import { CreateSizeDto } from './create-size.dto';
 
 export class UpdateSizeDto extends PartialType(CreateSizeDto) {
+  @IsNotEmpty()
   nameSize: string;
 }
--- src/size/size.service.ts
+++ src/size/size.service.ts
@@ -1,4 +1,8 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { CreateSizeDto } from './dto/create-size.dto';
@@ -10,6 +14,12 @@ export class SizeService {
   constructor(@InjectModel(Size.name) private sizeModel: Model<SizeDocument>) {}
 
   async create(createSizeDto: CreateSizeDto) {
+    // Filter out unwanted fields names
+    const filterBody = this.filterObj(createSizeDto, 'nameSize');
+    if (Object.keys(filterBody).length === 0) {
+      throw new BadRequestException('Fields are invalid');
+    }
+
     const createdSize = new this.sizeModel(createSizeDto);
     return await createdSize.save();
   }
@@ -43,10 +53,19 @@ export class SizeService {
   };
 
   async update(id: string, updateSizeDto: UpdateSizeDto) {
+    let updatedSize = await this.findOne(id);
+
     // Filter out unwanted fields names that are not allowed to be updated
     const filterBody = this.filterObj(updateSizeDto, 'nameSize');
+    if (Object.keys(filterBody).length === 0) {
+      throw new BadRequestException('Fields are invalid');
+    }
 
-    const updatedSize = await this.sizeModel.findByIdAndUpdate(id, filterBody);
+    updatedSize = await this.sizeModel.findByIdAndUpdate(id, updateSizeDto, {
+      new: true,
+      runValidators: true,
+    });
+
     return updatedSize;
   }
 
