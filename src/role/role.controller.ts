d5c9cc349d9b61ed8c55643fac5b755003114568
baosphoa0403 <baosphoa0403@gmail.com>
2021-07-19 16:09:25 +0700

    edit code swagger

--- src/role/role.controller.ts
+++ src/role/role.controller.ts
@@ -1,19 +1,20 @@
-import { Body, Controller, Post } from "@nestjs/common";
-import { ApiTags } from "@nestjs/swagger";
-import { CreateRoleDto } from "./dto/createRole.dto";
-import { Role } from "./entities/role.entity";
-import { RoleService } from "./role.service";
+import { Body, Controller, Post } from '@nestjs/common';
+import { ApiResponse, ApiTags } from '@nestjs/swagger';
+import { CreateRoleDto } from './dto/createRole.dto';
+import { Role } from './entities/role.entity';
+import { RoleService } from './role.service';
 
 @Controller('role')
-@ApiTags('role')
-export class RoleController{
-    
-    constructor(
-        private readonly roleService: RoleService
-    ){}
+@ApiTags('Role')
+export class RoleController {
+  constructor(private readonly roleService: RoleService) {}
 
-    @Post()
-    createRole(@Body() createRoleDto: CreateRoleDto):Promise<Role>{
-        return this.roleService.create(createRoleDto);
-    }
+  @Post()
+  @ApiResponse({
+    status: 201,
+    description: 'Created successfully role',
+  })
+  createRole(@Body() createRoleDto: CreateRoleDto): Promise<Role> {
+    return this.roleService.create(createRoleDto);
+  }
 }
