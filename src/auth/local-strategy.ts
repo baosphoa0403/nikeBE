f9c6f8a451a5bf3b343187e8e24fe03df4828d0c
baosphoa0403 <baosphoa0403@gmail.com>
2021-08-02 09:45:52 +0700

    add code more

--- src/auth/local-strategy.ts
+++ src/auth/local-strategy.ts
@@ -1,4 +1,4 @@
-import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { Strategy } from 'passport-local';
 import { AuthService } from './auth.service';
@@ -6,12 +6,11 @@ import { AuthService } from './auth.service';
 export class LocalStrategy extends PassportStrategy(Strategy) {
   constructor(private authService: AuthService) {
     super({ usernameField: 'email', passwordField: 'password' });
-    console.log(Strategy);
   }
   async validate(email: string, password: string): Promise<any> {
     const user = await this.authService.validateUser(email, password);
     if (!user) {
-      throw new UnauthorizedException();
+      throw new BadRequestException('Login Fail email or password wrong');
     }
     return user;
   }
